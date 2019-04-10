77a2bb0abccaa8999e1ba71bbc74bf7f893d6eae
cj summers <cjsummers003@hotmail.com>
2019-04-10 10:23:55 -0400

    new update

---
+++ build/precache-manifest.019f9413e00a0de6332088d4c1c8f33c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "f26005209d2040e67bce731fc323791b",
+    "url": "/static/media/darthvader.f2600520.png"
+  },
+  {
+    "revision": "69a0cac37d7db74d980e",
+    "url": "/static/css/main.3e813660.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b2693bfcf165c3e1ebe9aff395c5739d",
+    "url": "/static/media/back4.b2693bfc.jpg"
+  },
+  {
+    "revision": "e0191e4c2a0eafcaf95e9bcc20ccf703",
+    "url": "/static/media/ahsoka.e0191e4c.png"
+  },
+  {
+    "revision": "b93ec484c5da2fd56c3b90ce9ada794e",
+    "url": "/static/media/bobafett.b93ec484.png"
+  },
+  {
+    "revision": "6d05456eb7909abc6a3ece56e0d2510c",
+    "url": "/static/media/chewie.6d05456e.jpg"
+  },
+  {
+    "revision": "495f1f019030b95a6b7cd7d1fc24718e",
+    "url": "/static/media/darthmaul.495f1f01.png"
+  },
+  {
+    "revision": "69a0cac37d7db74d980e",
+    "url": "/static/js/main.d38cdaf4.chunk.js"
+  },
+  {
+    "revision": "3672643061e31acab8c06b28e23ab761",
+    "url": "/static/media/galacticEmpire.36726430.png"
+  },
+  {
+    "revision": "4407de10ed12ffc75d8862524e9f9a97",
+    "url": "/static/media/generalgrievous.4407de10.jpg"
+  },
+  {
+    "revision": "9c061ce62a3a2f8c254ef2f089ec0663",
+    "url": "/static/media/kyloren.9c061ce6.jpeg"
+  },
+  {
+    "revision": "888eddbc9203855b5937cf1e7662ba76",
+    "url": "/static/media/princessleia.888eddbc.jpg"
+  },
+  {
+    "revision": "bd9e03dbf01e65fba9b640a0ce10b544",
+    "url": "/static/media/rey.bd9e03db.png"
+  },
+  {
+    "revision": "38323f9d07479e5d365a2fe422a0840f",
+    "url": "/static/media/stormtrooper.38323f9d.jpg"
+  },
+  {
+    "revision": "2da34adc6f8850c014f356e4446e514a",
+    "url": "/static/media/yoda.2da34adc.png"
+  },
+  {
+    "revision": "d40e8eb540c1e2264653ef90e7edfbaa",
+    "url": "/static/media/youngobewan.d40e8eb5.jpg"
+  },
+  {
+    "revision": "3e79aeecfe8b818dc5bb8d16c485e0a4",
+    "url": "/index.html"
+  }
+];
